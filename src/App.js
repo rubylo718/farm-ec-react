5722d0700c57124c15efbe6dfe6a2732e675b634
rubylo718 <rubylo718@gmail.com>
2023-10-03 12:36:51 +0800

    feat: add login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
 import './styles/style.scss'
 
 function App() {
-	return <div className="App"></div>
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/login" element={<Login />}></Route>
+			</Routes>
+		</div>
+	)
 }
 
 export default App
---
+++ src/pages/Login.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleSubmit = () => {
+		console.log({username, password})
+	}
+	return (
+		<div className="container">
+			<div className="row justify-content-center align-items-center vh-100">
+				<div className="col-md-6 py-4 bg-light rounded-4">
+					<h1 className="h3 mb-4 font-weight-normal text-center">管理者登入</h1>
+					<div className="mb-2">
+						<label htmlFor="email" className="form-label w-100">
+							Email
+							<input
+								id="email"
+								className="form-control"
+								name="username"
+								type="email"
+								placeholder="Email Address"
+								value={username}
+								onChange={(e) => {setUsername(e.target.value)}}
+							/>
+						</label>
+					</div>
+					<div className="mb-2">
+						<label htmlFor="password" className="form-label w-100">
+							Password
+							<input
+								type="password"
+								className="form-control"
+								name="password"
+								id="password"
+								placeholder="password"
+								value={password}
+								onChange={(e)=> {setPassword(e.target.value)}}
+							/>
+						</label>
+					</div>
+					<button type="button" className="btn btn-secondary w-100 mb-2" onClick={handleSubmit}>
+						登入
+					</button>
+					<p className='mb-0 text-center'><Link to="/">回到前台</Link></p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Login
