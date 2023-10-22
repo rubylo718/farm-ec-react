32decccdba636c19816126f6c6c74f4107c6e13c
rubylo718 <rubylo718@gmail.com>
2023-10-22 15:42:02 +0800

    feat: add scroll to up on every transition

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { HashRouter } from 'react-router-dom'
+import ScrollToTop from './utils/scrollToTop'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		<HashRouter>
+			<ScrollToTop />
 			<App />
 		</HashRouter>
 	</React.StrictMode>
