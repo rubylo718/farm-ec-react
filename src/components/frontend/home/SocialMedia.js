dcf86ae1ba2f0465c3a94f920f55ce0e4dde990a
rubylo718 <rubylo718@gmail.com>
2024-01-02 15:20:00 +0800

    style: adjust icon and button color of social media section on front page

--- src/components/frontend/home/SocialMedia.js
+++ src/components/frontend/home/SocialMedia.js
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom'
+import { faFacebook, faLine } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const SocialMedia = () => {
 	const GITHUB_URL = 'https://github.com/rubylo718/farm-ec-react'
@@ -8,7 +10,13 @@ const SocialMedia = () => {
 				<div className="col-sm-6 my-2 mb-sm-0 d-flex justify-content-center">
 					<div className="card text-center">
 						<div className="card-body">
-							<h3 className="card-title fs-4">輕鬆加 Line 訂購水果</h3>
+							<h3 className="card-title fs-4">
+								輕鬆加{' '}
+								<span className="color-line">
+									<FontAwesomeIcon icon={faLine} />
+								</span>{' '}
+								來訂購水果
+							</h3>
 							<p className="card-text text-muted">
 								掃描行動條碼，即可將官方帳號加入好友！
 							</p>
@@ -18,7 +26,7 @@ const SocialMedia = () => {
 								to={GITHUB_URL}
 								target="_blank"
 							>
-								點我立即加入好友
+								點我加 Line 好友
 							</Link>
 						</div>
 					</div>
@@ -26,17 +34,23 @@ const SocialMedia = () => {
 				<div className="col-sm-6 my-2 mb-sm-0 d-flex justify-content-center">
 					<div className="card text-center">
 						<div className="card-body">
-							<h3 className="card-title fs-4">作伙來 FB 小農粉絲團</h3>
+							<h3 className="card-title fs-4">
+								作伙來{' '}
+								<span className="color-fb">
+									<FontAwesomeIcon icon={faFacebook} />
+								</span>{' '}
+								小農粉絲團
+							</h3>
 							<p className="card-text text-muted">
 								得知小農最新商品，以及不定期限時優惠！
 							</p>
 							<Link
 								role="button"
-								className="btn btn-info text-white"
+								className="btn btn-primary text-white"
 								to={GITHUB_URL}
 								target="_blank"
 							>
-								點我進入粉絲專頁
+								點我去 FB 粉絲團
 							</Link>
 						</div>
 					</div>
